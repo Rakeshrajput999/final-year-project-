33d088a604c9774601ee5d12e2934d1c19e472c5
Rakeshrajput999 <baajigarrakesh@gmail.com>
2021-12-09 09:55:27 +0530

    dec9

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var RegisterChecks =require("../middleware/RegisterChecks")
+var Register =require("../controllers/Register")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/** 
+ * @description {email first name lastname  paswword conferm password } = req.body
+ * security performance edge cses
+ * level 1
+ * email validaton 
+ * password validaton 
+ * password conferm password same 
+ * lecel 2
+ * 
+ * javascript and sql injection 
+ * level 3
+ * check if email already exist 
+ * emaail.lowercase
+ * save 
+ * 
+ */
+
+router.post('/register',RegisterChecks,Register)
 
 module.exports = router;
