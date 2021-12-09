088e4791468e0dad45376c82b875c451716563be
Rakeshrajput999 <baajigarrakesh@gmail.com>
2021-12-09 08:20:16 +0530

    9dec

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -24,3 +24,4 @@ const Card = () => {
 export default Card
 
    
+ 
--- client/src/Components/Department.jsx
+++ client/src/Components/Department.jsx
@@ -1,24 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-const routes = [
-  {
-    path: "/",
-    exact: true,
-    sidebar: () => <div>home!</div>,
-    main: () => <h2>Home</h2>,
-  },
-  {
-    path: "/bubblegum",
-    sidebar: () => <div>bubblegum!</div>,
-    main: () => <h2>Bubblegum</h2>,
-  },
-  {
-    path: "/shoelaces",
-    sidebar: () => <div>shoelaces!</div>,
-    main: () => <h2>Shoelaces</h2>,
-  },
-];
+
 const Department = () => {
     return (
         <div className={`tw-pl-1`}>
